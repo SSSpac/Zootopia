1a5144d116a804d3f39d3672b47ffc5765721e44
Andrew Julius Kalumba <andrewkalumba@MacBook-Air-som-tillhor-Andrew.local>
2024-11-25 11:00:41 +0100

    added another css file called main content

--- scripts/zoo.js
+++ scripts/zoo.js
@@ -1,5 +1,6 @@
 //alert("welcome")
 const toggleButton = document.getElementById('toggle-btn')
+//const toggleButton = document.querySelector('#toggle-btn').addEventListener
 const sidebar = document.getElementById('sidebar')
 
 function toggleSidebar(){
@@ -30,3 +31,17 @@ function closeAllSubMenus(){
     ul.previousElementSibling.classList.remove('rotate')
   })
 }
+
+window.addEventListener("load", function() {
+  setTimeout(
+    function open(event) {
+      document.querySelector(".popup").style.display = "block";
+    },
+    1000
+  )
+})
+document.querySelector("#close").addEventListener
+("click", function() {
+  document.querySelector(".popup").style.display = "none";
+}
+)
