df9bff9a78ae2f575245e699dd8cbf8a0ffc0626
Maatala <mikael.m.sand@gmail.com>
2024-12-04 13:54:34 +0100

    progress with html and sidebar

--- birds/js/birds.js
+++ birds/js/birds.js
@@ -53,7 +53,7 @@ button.addEventListener("click", (event) => {
     event.preventDefault();
 
     const container = document.getElementById('bird-info-container');
-    container.innerHTML = '<h2>Hi and Welcome to the Birds page, we have three different birds here at the Zoo, to find more information about them, click on the button on the life side in the sidebar, to find out more!</h2>';
+    container.innerHTML = '<h2>Hi and Welcome to the Birds page, we have three different birds here at the Zoo, to find more information about them, click on the button on the left side in the sidebar, to find out more!</h2>';
 
     const viewBirdsButton = document.createElement('button');
     viewBirdsButton.textContent = "View Birds";
@@ -104,4 +104,35 @@ function myFunction(button) {
         button.innerHTML = "Read less";
         moreText.style.display = "inline";
     }
+}
+
+const sidebar = document.getElementById('sidebar')
+
+function toggleSidebar(){
+  sidebar.classList.toggle('close') 
+  toggleButton.classList.toggle('rotate')
+
+  closeAllSubMenus()
+}
+
+function toggleSubMenu(button){
+
+  if(!button.nextElementSibling.classList.contains('show')){
+    closeAllSubMenus()
+  }
+
+  button.nextElementSibling.classList.toggle('show')
+  button.classList.toggle('rotate')
+
+  if(sidebar.classList.contains('close')){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+  }
+}
+
+function closeAllSubMenus(){
+  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+    ul.classList.remove('show')
+    ul.previousElementSibling.classList.remove('rotate')
+  })
 }
