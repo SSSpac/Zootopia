5e352c067952297cb343e58e93840c797dca1df2
Maatala <mikael.m.sand@gmail.com>
2024-12-09 18:48:51 +0100

    connecting links

--- scripts/birds.js
+++ scripts/birds.js
@@ -125,42 +125,10 @@ document.querySelectorAll('a:not(#birds-button)').forEach(link => {
 });
 
 const sidebarButtons = document.querySelectorAll('.animal-button');
-sidebarButtons.forEach(button => {
-button.addEventListener('click', (event) => {
-  const birdName = event.target.getAttribute('data-animal');
-  displaySpecificBirdInfo(birdName);
-  onClick.showWelcomeMsg = false;
-});
-});
-
-const sidebar = document.getElementById('sidebar');
-const toggleButton = document.getElementById('toggle-sidebar-btn');
-
-function toggleSidebar() {
-  sidebar.classList.toggle('close');
-  toggleButton.classList.toggle('rotate');
-  closeAllSubMenus();
-}
-
-function toggleSubMenu(button) {
-  if (!button.nextElementSibling.classList.contains('show')) {
-      closeAllSubMenus();
-  }
-
-  button.nextElementSibling.classList.toggle('show');
-  button.classList.toggle('rotate');
-
-  if (sidebar.classList.contains('close')) {
-      sidebar.classList.toggle('close');
-      toggleButton.classList.toggle('rotate');
-  }
-}
-
-function closeAllSubMenus() {
-  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
-      ul.classList.remove('show');
-      ul.previousElementSibling.classList.remove('rotate');
+  sidebarButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    const birdName = event.target.getAttribute('data-animal');
+    displaySpecificBirdInfo(birdName);
+    onClick.showWelcomeMsg = false;
   });
-}
-
-toggleButton.addEventListener('click', toggleSidebar);
+});
