7556f011b5bdecdb40a1f8387fb86989a0199b96
SSSpac <saifeisa820@gmail.com>
2025-01-08 10:13:55 +0100

    Update Home-page.js

--- js/Home-page.js
+++ js/Home-page.js
@@ -104,7 +104,7 @@ const animals = {
   },
   Kookaburra: {
     Name: "Kookaburra",
-    Image: "./images/kooKaburra.jpg",
+    Image: "./images/Kookaburra.jpg",
     Information: "The laughing kookaburra is the largest member of the Kingfisher family, native to Australia's eucalyptus forests.",
     Food: "Insects and small animals",
     Lifespan: "Around 20 years",
